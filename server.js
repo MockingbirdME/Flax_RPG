0365ff6024f6462664a2e3ed1737734595e9f96e
devon-henegar <devon.henegar@ibm.com>
2019-04-14 15:14:59 -0400

    Removed server logs.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const DOCUMENTATION = require('./documentation/documentation.js');
+const DOCUMENTATION = require('./documentation.js');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,9 +7,6 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-console.log(DOCUMENTATION);
-
-console.log("TEST");
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT booyah' });
