e5a4d97926a98239000bdc3346d41408dab72a78
devon-henegar <devon.henegar@ibm.com>
2019-04-14 13:45:39 -0400

    restart generate documentation work.

--- documentation/documentation.js
+++ documentation/documentation.js
@@ -5,30 +5,17 @@ const path = require('path');
 const CHAPTERS_DIRECTORY = path.resolve(__dirname, '../documentation');
 
 // TODO test if this can just be exported directly with no variable needed.
-let documentation = processDirectoryContents(CHAPTERS_DIRECTORY, 1);
+let documentation = {
+    documentation: ""
+};
+
+processDirectoryContents(CHAPTERS_DIRECTORY, 'documentation', 1);
 
 // To process the directory.
-function processDirectoryContents(directory, depth) {
-    // Store contents in an Object.
-    let content = {
-        depth: depth
-    };
+function processDirectoryContents(directory, path, depth) {
+    console.log(directory, depth);
 
-    let nextDepth = depth + 1;
-    // For each item(file or folder) in the directory...
-    fs.readdirSync(directory, 'utf8').forEach(item => {
-        /** If it's a text.md file save the contents as content.text. */
-        if (item === 'text.md') content.text = fs.readFileSync(path.resolve(__dirname, directory, item), 'utf8');
-        // If it's a javascript file assing the obejct it returns to contents.
-        // TODO consider adding some saftey check to ensure no existing properties are being replaced.
-        if (path.extname(item) === '.js') {
-            Object.assign(content, require(path.resolve(__dirname, directory, item)));
-        }
-        /** If it's a directory process its contents and save them as content.<directory name>. */
-        if (!item.includes('.')) content[item] = processDirectoryContents(path.resolve(__dirname, directory, item), nextDepth);
-    });
 
-    return content;
 }
 
 module.exports = documentation;
