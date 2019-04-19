3e0e3da54e022083e4774f7228ec9eeb1e6e89e2
devon-henegar <devon.henegar@ibm.com>
2019-04-19 17:34:53 -0400

    Gave chapterList functionality.

--- client/src/topNav/chapterList.js
+++ client/src/topNav/chapterList.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
+import { observer, inject } from "mobx-react";
 
+
+const ChapterList = inject("rootStore")(observer(
 class ChapterList extends Component {
     render() {
-
+        console.log(this.props.rootStore.rulesStore.chapterList);
+        const chapters = this.props.rootStore.rulesStore.chapterList.map(title => {
+            let link = `/core rules/${title}`;
+            return (
+                <div key={title} className="chapterLink" >
+                    <a href={link} >{title}</a>
+                </div>
+            )
+        })
         return (
             <div>
-
+                {chapters}
             </div>
         )
     }
 }
+))
 
 export default ChapterList;
