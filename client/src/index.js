1498502b99b1624baaac61a128502f0a4a8d1246
devon-henegar <devon.henegar@ibm.com>
2019-04-19 14:01:17 -0400

    Index now doing index's job of just being an index, like indexes do.

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
-let tests = () => {return <h1>TEST TEST TEST</h1>};
+import Routes from './routes.js'
+import Header from './header/header.js';
+import TopNav from './topNav/topNav.js';
+import App from './App';
 
-    ReactDOM.render(
-        <Router basename={'/'}>
-            <App />
-            <Route path='/test' component={tests} />
-        </Router>,
-        document.getElementById('root')
-    );
+import './index.css';
 
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: https://bit.ly/CRA-PWA
-    serviceWorker.unregister();
+
+ReactDOM.render(
+    <Router basename={'/'}>
+        <Header />
+        <TopNav />
+        <Routes />
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
