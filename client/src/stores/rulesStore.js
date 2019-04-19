94a2f87d678cc5e77673d81a536fdc412e2d3a8a
devon-henegar <devon.henegar@ibm.com>
2019-04-19 16:01:39 -0400

    Rules store now self sufficient.

--- client/src/stores/rulesStore.js
+++ client/src/stores/rulesStore.js
@@ -1,30 +1,27 @@
 import { observable, action, decorate } from "mobx";
 
 class RulesStore {
-  constructor(rootStore) {
-    this.rootStore = rootStore;
-    this.callDocumentationAPI();
-  }
-  rawDocs = {};
-  chapters = [];
-  test = "THIS IS A TEST STRING."
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+        this.callDocumentationAPI();
+    }
+    rawDocs = {};
+    test = "THIS IS A TEST STRING."
 
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callDocumentationAPI = async () => {
-      const response = await fetch('/documentation');
-      const body = await response.json();
-      if (response.status !== 200) {
-          throw Error(body.message)
-      }
-      this.rawDocs = body.DOCUMENTATION;
-      console.log(Object.keys(body.DOCUMENTATION));
-      // return body;
-  };
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callDocumentationAPI = async () => {
+        const response = await fetch('/documentation');
+        const body = await response.json();
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        this.rawDocs = body.DOCUMENTATION;
+        this.chapterList = this.parseChapters(body.DOCUMENTATION)
+    };
 
 }
 
 export default decorate(RulesStore, {
     rawDocs: observable,
-    test: observable,
-    callDocumentationAPI: action
+    test: observable
 });
