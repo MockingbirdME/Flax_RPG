567f6c0d2647a85c03d0ed8d4885e011053ec286
devon-henegar <devon.henegar@ibm.com>
2019-04-19 14:02:34 -0400

    added rules tools class.

---
+++ client/src/topNav/rulesTools.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class RulesTools extends Component {
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default RulesTools;
