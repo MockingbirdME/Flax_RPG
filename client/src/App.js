cfaf0caa0d07fbdeb7997bd364dede4e25eb7ae3
devon-henegar <devon.henegar@ibm.com>
2019-04-19 15:41:30 -0400

    utilized RulesStore in app.

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,44 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+
+import { observer, inject } from "mobx-react";
+
 library.add(faChevronDown, faChevronUp, faChevronRight)
 
+const App = inject("rootStore")(observer(
 class App extends Component {
     state = {
-        documentation: "Documentation did not load correctly."
+        documentation: "Documentation did not load correctly.",
     };
 
-    componentDidMount() {
-        // Call our fetch function below once the component mounts
-        this.callDocumentationAPI()
-        .then(res => {
-            this.setState({ documentation: res.DOCUMENTATION });
-            console.log(this.state.documentation);
-        })
-        .catch(err => console.log(err));
+    componentWillMount() {
+        // this.props.rootStore.rulesStore.callDocumentationAPI();
     }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callDocumentationAPI = async () => {
-        const response = await fetch('/documentation');
-        const body = await response.json();
-        if (response.status !== 200) {
-            throw Error(body.message)
-        }
-        return body;
-    };
+
+   //  componentDidMount() {
+   //     // Call our fetch function below once the component mounts
+   //     this.callDocumentationAPI()
+   //     .then(res => {
+   //         this.setState({ documentation: res.DOCUMENTATION });
+   //         console.log(this.state.documentation);
+   //     })
+   //     .catch(err => console.log(err));
+   // }
+   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+   // callDocumentationAPI = async () => {
+   //     const response = await fetch('/documentation');
+   //     const body = await response.json();
+   //     if (response.status !== 200) {
+   //         throw Error(body.message)
+   //     }
+   //     return body;
+   // };
+
 
     renderedDoc = () => {
         // If documentation isn't loaded, return.
-        if (typeof this.state.documentation === 'string') return;
+        // if (typeof this.state.documentation === 'string') return;
         // Otherwise get the location.
         let location = this.props.location.pathname.toLowerCase();
         // Remove the leading '/' from the location.
@@ -44,10 +53,15 @@ class App extends Component {
         // If no location is provided assume a default.
         if (!location) location = 'Core Rules';
 
-        return this.state.documentation[location];
+        return this.props.rootStore.rulesStore.rawDocs[location];
     }
 
     render() {
+        console.log(this.props);
+        console.log(this.props.rootStore); console.log(this.props.rootStore.rulesStore.rawDocs);
+        console.log(Object.keys(this.props.rootStore.rulesStore.rawDocs));
+        const test = this.props.rootStore.rulesStore.test || "";
+        console.log(test);
         return (
             <div className="App">
                 <header className="App-header">
@@ -67,5 +81,6 @@ class App extends Component {
         );
     }
 }
+))
 
 export default withRouter(App);
