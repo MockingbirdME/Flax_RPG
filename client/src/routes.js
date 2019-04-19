19227eb5a770451486b5edfd9dbe969ffbcf725b
devon-henegar <devon.henegar@ibm.com>
2019-04-19 17:33:28 -0400

    Route now invoking documentation.

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
+import CoreRules from "./documentation/coreRules.js";
 
 class Routes extends Component {
     render() {
         return(
             <div id="route-controller" >
-
+                <Switch>
+                    <Route path="/core rules" component={CoreRules} />
+                </Switch>
             </div>
         )
     }
