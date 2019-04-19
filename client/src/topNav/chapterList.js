e68b6fb0f0a52e95d52b1bb67fb16ec6fb1097f6
devon-henegar <devon.henegar@ibm.com>
2019-04-19 17:35:23 -0400

    Removed development logs.

--- client/src/topNav/chapterList.js
+++ client/src/topNav/chapterList.js
@@ -5,7 +5,6 @@ import { observer, inject } from "mobx-react";
 const ChapterList = inject("rootStore")(observer(
 class ChapterList extends Component {
     render() {
-        console.log(this.props.rootStore.rulesStore.chapterList);
         const chapters = this.props.rootStore.rulesStore.chapterList.map(title => {
             let link = `/core rules/${title}`;
             return (
