107499b32c7bfb7063f1f9a6d9ae564e9d875c41
devon-henegar <devon.henegar@ibm.com>
2019-04-19 14:02:24 -0400

    Expanded Nav now has children.

--- client/src/topNav/extendedNav.js
+++ client/src/topNav/extendedNav.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
+import ChapterList from './chapterList.js';
+import RulesTools from './rulesTools.js';
 
 class ExtendedNav extends Component {
     render() {
-        let hoveredClass = this.props.hoverDisplay === this.props.title ? "extendedNav__container" : "hidden"
-
+        console.log(this.props.hoverDisplay, this.props.hoverDisplay === this.props.title);
+        let hoveredClass = this.props.hoverDisplay === this.props.title ? "extendedNav__container" : "hidden";
+        hoveredClass = 'extendedNav__container'
+        console.log('class:', hoveredClass);
         return (
             <div
                 className={hoveredClass}
                 onMouseOver={() => this.props.onHover(this.props.title)}
                 onMouseOut={() => this.props.onHover(null)}
             >
+                <ChapterList />
+                <RulesTools />
             </div>
         )
     }
