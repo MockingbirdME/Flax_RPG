0d00793e99c6dcb580ea34713ea9f1847c33b131
devon-henegar <devon.henegar@ibm.com>
2019-04-21 08:45:08 -0400

    adding primary attributes to documentation from data structure.

--- documentation/1 - Character Statistics/1 - Primary Attributes/text.js
+++ documentation/1 - Character Statistics/1 - Primary Attributes/text.js
@@ -1,3 +1,9 @@
+let data = require('../../../data/primary_attributes.js');
+
 let markdown = `Character’s have four primary attributes that describe their physical and mental talents. Primary attributes play a key role in determining a character’s potential for success in various actions.`;
+
+Object.keys(data).forEach(attribute => {
+    markdown += `\n#### ${attribute.charAt(0).toUpperCase() + attribute.slice(1)}\n${data[attribute].description}`;
+});
 
 module.exports = markdown;
