44ac8c2d3c67604526cee54b2d762508c944ab64
devon-henegar <devon.henegar@ibm.com>
2019-04-21 08:44:39 -0400

    added data structure for primary attributes.

---
+++ data/primary_attributes.js
@@ -0,0 +1,16 @@
+let primaryAttributesData = {
+    body: {
+        description: "a character’s strength, stamina, and toughness."
+    },
+    reflexes: {
+        description: "how quickly a character moves or reacts."
+    },
+    perception: {
+        description: "how well tuned the character's senses are."
+    },
+    mind: {
+        description: "a character’s learning and mental discipline."
+    }
+};
+
+module.exports = primaryAttributesData;
