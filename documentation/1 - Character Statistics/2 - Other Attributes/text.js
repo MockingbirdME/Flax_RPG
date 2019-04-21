810893e6b4522c64f4559773c8638188fce1fd0f
devon-henegar <devon.henegar@ibm.com>
2019-04-21 08:45:14 -0400

    adding other attributes to documentation from data structure.

--- documentation/1 - Character Statistics/2 - Other Attributes/text.js
+++ documentation/1 - Character Statistics/2 - Other Attributes/text.js
@@ -1,3 +1,9 @@
+let data = require('../../../data/other_attributes.js');
+
 let markdown = `These statistics and resources are derived from the character’s strain, primary attributes, and skills.`;
+
+Object.keys(data).forEach(attribute => {
+    markdown += `\n#### ${data[attribute].displayName}\n\n  *Description:* ${data[attribute].description}\n\n  *Base Value:* ${data[attribute].baseValue}`;
+});
 
 module.exports = markdown;
