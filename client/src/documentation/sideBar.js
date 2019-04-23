c1a1249ad1faae3da9bbafc4ad6b2a110ac8286c
devon-henegar <devon.henegar@ibm.com>
2019-04-23 18:04:13 -0400

    Added documentation sideBar.

---
+++ client/src/documentation/sideBar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+class SideBar extends Component {
+    render() {
+
+        return (
+            <div className="sideBar">
+            <div style={{"margin-top": "50%"}}>
+            <p>Nothing Here Yet,</p>
+            <p>Content Coming Soon.</p>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default SideBar;
