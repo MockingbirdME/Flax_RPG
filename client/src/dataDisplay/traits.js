1ac3ad6eaa9e50c2109718e4cbb9bc31f8e074e5
devon-henegar <devon.henegar@ibm.com>
2019-04-23 18:53:59 -0400

    Traits invokes dataDisplay.

---
+++ client/src/dataDisplay/traits.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import DataDisplay from './dataDisplay.js';
+
+class Traits extends Component {
+    render() {
+
+        return (
+            <DataDisplay
+                name="Traits"
+            />
+        )
+    }
+}
+
+export default Traits;
