029718b51bf82faf353e09d10591d6e65495d732
devon-henegar <devon.henegar@ibm.com>
2019-04-23 15:24:57 -0400

    Invoked toolbuttons.

--- client/src/topNav/rulesTools.js
+++ client/src/topNav/rulesTools.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import ToolButton from './toolButton.js';
 
 class RulesTools extends Component {
     render() {
 
         return (
             <div className="rulesToolsContainer">
-
+                <ToolButton
+                    name="TEST 1"
+                    link="/test1"
+                />
+                <ToolButton
+                    name="TEST 2"
+                    link="/test2"
+                />
+                <ToolButton
+                    name="TEST 3"
+                    link="/test3"
+                />
+                <ToolButton
+                    name="TEST 4"
+                    link=""
+                />
             </div>
         )
     }
