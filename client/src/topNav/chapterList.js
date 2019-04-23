51ce9afc441b87b6801a33a1b98713928253c545
devon-henegar <devon.henegar@ibm.com>
2019-04-23 11:18:21 -0400

    Separated chapters into halves

--- client/src/topNav/chapterList.js
+++ client/src/topNav/chapterList.js
@@ -5,17 +5,26 @@ import { observer, inject } from "mobx-react";
 const ChapterList = inject("rootStore")(observer(
 class ChapterList extends Component {
     render() {
-        const chapters = this.props.rootStore.rulesStore.chapterList.map(title => {
+        let numberOfChapters = this.props.rootStore.rulesStore.chapterList.length;
+        let chapterNumberCutOff = Math.round(numberOfChapters/2);
+
+        let chaptersFirstHalf = [];
+        let chaptersSecondHalf = [];
+
+        this.props.rootStore.rulesStore.chapterList.forEach((title, index) => {
             let link = `/core rules/${title}`;
-            return (
+            let returnValue = (
                 <div key={title} className="chapterLink" >
                     <a href={link} >{title}</a>
                 </div>
             )
+            if (index < chapterNumberCutOff)  chaptersFirstHalf.push(returnValue);
+            else chaptersSecondHalf.push(returnValue);
         })
         return (
-            <div>
-                {chapters}
+            <div className="chapterSectionsContainer">
+                <div>{chaptersFirstHalf}</div>
+                <div>{chaptersSecondHalf}</div>
             </div>
         )
     }
