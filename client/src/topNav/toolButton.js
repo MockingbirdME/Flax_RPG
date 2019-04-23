34b96b8c61f7c6b94dd1a97c5093a3a8e6102443
devon-henegar <devon.henegar@ibm.com>
2019-04-23 15:24:31 -0400

    Added toolButton Display.

---
+++ client/src/topNav/toolButton.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+class ToolButton extends Component {
+    onClick = () => {
+        console.log('CLicked');
+    }
+    render() {
+
+        return (
+            <div className="toolButton" onClick={this.onClick}>
+                <Link to={this.props.link}>
+                    <h3>
+                        {this.props.name}
+                    </h3>
+                </Link>
+            </div>
+        )
+    }
+}
+
+export default ToolButton;
