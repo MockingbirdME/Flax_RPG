d1652d3042503c66fbf4dcba9e0396632b6b7ba9
devon-henegar <devon.henegar@ibm.com>
2019-04-24 16:07:17 -0400

    Scafolded dataDisplaySearchSection component.

---
+++ client/src/dataDisplay/dataDisplaySearchSection.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class DataDisplaySearchSection extends Component {
+    render() {
+
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default DataDisplaySearchSection;
