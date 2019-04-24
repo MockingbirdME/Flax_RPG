8e2ac180ca8cc8a8c292b07e6032885456ecba11
devon-henegar <devon.henegar@ibm.com>
2019-04-24 17:50:59 -0400

    Began work on dataDisplayFilterSection.

--- client/src/dataDisplay/dataDisplayFilterSection.js
+++ client/src/dataDisplay/dataDisplayFilterSection.js
@@ -2,11 +2,41 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class DataDisplayFilterSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchedName: ""
+        };
+    }
+
+    onChange(value) {
+        this.setState({searchedName: value});
+    }
     render() {
 
         return (
-            <div className="dataDisplay__filter__Container">
-                <
+            <div className="dataDisplay__filter__container">
+                <div className="dataDisplay__filter__name">
+                    <h5>{this.props.nameSingular} Name</h5>
+                    <input
+                      className="dataDisplay__filter__name__input"
+                      type={"text"}
+                      placeholder={"placeholder text"}
+                      value={this.state.searchedName}
+                      onChange={ev => this.onChange(ev.target.value)}
+                    />
+                </div>
+                <div className="dataDisplay__filter__checkBox">
+                    <h5>Placeholder Title</h5>
+                </div>
+                <div className="dataDisplay__filter__checkBox">
+                    <h5>Placeholder Title</h5>
+                </div>
+                <div>
+                    <div  className="dataDisplay__filter__button">
+                        <h5>Filter {this.props.namePlural}</h5>
+                    </div>
+                </div>
             </div>
         )
     }
