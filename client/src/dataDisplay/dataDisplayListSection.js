570c45e6d88cceaf242b3a4a42be9cb5ab5fb4d5
devon-henegar <devon.henegar@ibm.com>
2019-04-24 19:14:59 -0400

    Scafolded dataDisplayListSection.

---
+++ client/src/dataDisplay/dataDisplayListSection.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class DataDisplayListSection extends Component {
+    render() {
+
+        return (
+            <div className="dataDisplay__title__container">
+
+            </div>
+        )
+    }
+}
+
+export default DataDisplayListSection;
