a8d0fba7675fe40eb094a9ac90d5e2c50347ea2c
devon-henegar <devon.henegar@ibm.com>
2019-04-24 21:57:31 -0400

    Styled dataDisplayListSection html.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
