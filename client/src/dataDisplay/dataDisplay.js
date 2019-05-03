fd8a9360c6896a603ac2df4ad80f09a3e2196b11
devon-henegar <devon.henegar@ibm.com>
2019-05-03 15:44:19 -0400

    Data display now tracks filter params.

--- client/src/dataDisplay/dataDisplay.js
+++ client/src/dataDisplay/dataDisplay.js
@@ -8,7 +8,19 @@ import DataDisplayListSection from './dataDisplayListSection.js';
 import { observer, inject } from "mobx-react";
 
 const DataDisplay = inject("rootStore")(observer(
-class CoreRules extends Component {
+class DataDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterName: "",
+            filterFields: {}
+        };
+    }
+
+    updateFilters(data) {
+        if (typeof data === "string") this.setState({filterName: data});
+        else this.setState({filterFields: data});
+    }
 
     render() {
         return (
@@ -20,10 +32,15 @@ class CoreRules extends Component {
                 <DataDisplayFilterSection
                     nameSingular={this.props.nameSingular}
                     namePlural={this.props.namePlural}
+                    filterableFields={this.props.filterableFields}
+                    data={this.props.data}
+                    updateFilters={ev => this.updateFilters(ev)}
                 />
                 <DataDisplayListSection
                     data={this.props.data}
                     fields={this.props.sortableFields}
+                    filterName={this.state.filterName}
+                    filterFields={this.state.filterFields}
                 />
             </div>
         );
