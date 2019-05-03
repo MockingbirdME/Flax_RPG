05a800c7d77875aef74b1df29dde5a6c50e68512
devon-henegar <devon.henegar@ibm.com>
2019-05-03 16:51:41 -0400

    Make multiple columns of checkboxes.

--- client/src/dataDisplay/dataDisplayFilterField.js
+++ client/src/dataDisplay/dataDisplayFilterField.js
@@ -24,10 +24,24 @@ class DataDisplayFilterField extends Component {
             this.props.fields.map((field, index) => <div key={index}><input type="checkbox" value={field} onChange={ev => this.changeFilter({field})} checked={this.shouldBeChecked(field)}/>{field}<br /></div>)
         )
 
+        let divCount = Math.ceil(options.length / 4);
+        if (divCount > 3) divCount = 3;
+        let divCountLength = options.length/divCount;
+        console.log(divCount);
+        let optionDivs = [];
+        for (let i = 0; i < divCount; i++) {
+            optionDivs.push(<div key={i}>
+                {options.splice(0, divCountLength)}
+            </div>)
+        }
+        console.log(optionDivs);
         return (
-            <div className="dataDisplay__filter__checkBox">
+            <div
+                className="dataDisplay__filter__checkBox"
+                style={{width: this.props.width}}
+                >
                 <h5>{this.props.name}</h5>
-                {options}
+                <div className="checkmark__container">{optionDivs}</div>
             </div>
         )
     }
