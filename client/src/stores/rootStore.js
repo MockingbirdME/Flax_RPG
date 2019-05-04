c9fc01e412c93d8da734d2311fc8024b78b804bc
devon-henegar <devon.henegar@ibm.com>
2019-05-04 15:29:44 -0400

    Added Strain store.

--- client/src/stores/rootStore.js
+++ client/src/stores/rootStore.js
@@ -1,9 +1,11 @@
 import RulesStore from "./rulesStore.js";
+import StrainsStore from "./strainsStore.js";
 import TraitsStore from "./traitsStore.js";
 
 export class RootStore {
   constructor() {
       this.rulesStore = new RulesStore(this);
+      this.strainsStore = new StrainsStore(this);
       this.traitsStore = new TraitsStore(this);
   }
 }
