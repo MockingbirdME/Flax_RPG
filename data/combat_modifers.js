36e17d2355d73036ba34c3da47cc8061ad05e821
devon-henegar <devon.henegar@ibm.com>
2019-05-06 16:14:16 -0400

    Added combat modifiers data.

---
+++ data/combat_modifers.js
@@ -0,0 +1,40 @@
+let primaryAttributesData = {
+    kneeling: {
+        displayName: "Kneeling",
+        description: "Targeting a kneeling character with a ranged attack grants one level of disadvantage, targeting a kneeling character with a melee attack grants one level of advantage."
+    },
+    prone: {
+        displayName: "Prone",
+        description: "Targeting a prone character with a ranged attack grants two levels of disadvantage, targeting a prone character with a melee attack grants two levels of advantage."
+    },
+    cover: {
+        displayName: "Cover",
+        description: ""
+    },
+    concealment: {
+        displayName: "Concealment",
+        description: ""
+    },
+    flanked: {
+        displayName: "Flanked",
+        description: "A character is flanked if they have enemies on either side of them who are in range to make melee attacks agains the character. All melee attacks agains a flanked character gain a level of advantage."
+    },
+    flatFooted: {
+        displayName: "Flat Footed",
+        description: "A flat footed target is in no way prepared for combat, in addition to being surprised all attacks against a flat footed character gain one level of advantage."
+    },
+    surprised: {
+        displayName: "Surprised",
+        description: "A surprised target does not benefit from melee, ranged, or magic defense bonuses and doesn't act on the round they are surprised."
+    },
+    wind: {
+        displayName: "Wind",
+        description: ""
+    },
+    reach: {
+        displayName: "Reach",
+        description: ""
+    }
+};
+
+module.exports = primaryAttributesData;
