e53347cd3b0a291a036366a51b9e16d562ef338f
devon-henegar <devon.henegar@ibm.com>
2019-05-07 16:24:18 -0400

    added combat action store to root store.

--- client/src/stores/rootStore.js
+++ client/src/stores/rootStore.js
@@ -1,4 +1,5 @@
 import RulesStore from "./rulesStore.js";
+import CombatActionsStore from "./combatActionsStore.js";
 import SkillChecksStore from "./skillChecksStore.js";
 import StrainsStore from "./strainsStore.js";
 import TraitsStore from "./traitsStore.js";
@@ -6,6 +7,7 @@ import TraitsStore from "./traitsStore.js";
 export class RootStore {
   constructor() {
       this.rulesStore = new RulesStore(this);
+      this.combatActionsStore = new CombatActionsStore(this);
       this.skillChecksStore = new SkillChecksStore(this);
       this.strainsStore = new StrainsStore(this);
       this.traitsStore = new TraitsStore(this);
