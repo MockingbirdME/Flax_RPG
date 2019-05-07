47fa6678c5ea10459eb764f789755bd0dc1c074b
devon-henegar <devon.henegar@ibm.com>
2019-05-07 16:02:30 -0400

    Added combat actions route.

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import CoreRules from "./documentation/coreRules.js";
 import SkillChecks from "./dataDisplay/skillChecks.js";
+import CombatActions from "./dataDisplay/combatActions.js";
 import Strains from "./dataDisplay/strains.js";
 import Traits from "./dataDisplay/traits.js";
 
@@ -13,6 +14,7 @@ class Routes extends Component {
                 <Switch>
                     <Route path="/core rules" component={CoreRules} />
                     <Route path="/skill checks" component={SkillChecks} />
+                    <Route path="/combat actions" component={CombatActions} />
                     <Route path="/strains" component={Strains} />
                     <Route path="/traits" component={Traits} />
                 </Switch>
