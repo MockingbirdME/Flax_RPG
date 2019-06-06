1e8e3d7bbd5b009895e3e6bdebc57c1cf64c95f7
Devon Henegar <devon@Devons-MacBook-Pro.local>
2019-06-06 07:29:12 -0400

    Let => const

--- documentation/04 - Traits/html.js
+++ documentation/04 - Traits/html.js
@@ -1,4 +1,4 @@
-let html = `
+const html = `
     <p>Traits have a number of properties:
     <ul>
     <li><b>Requirements:</b> some traits have one or more requirements, these may be a specific strain limitation, some number of other traits with a shared keyword, or another specific trait(s).</li>
