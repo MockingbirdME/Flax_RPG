fb4eb37cbe7b1f419c6d56184939a145e175e872
Devon Henegar <devon@Devons-MacBook-Pro.local>
2019-06-08 18:57:33 -0400

    More character sheet concept work.

--- client/src/characterSheet/characterSheet.js
+++ client/src/characterSheet/characterSheet.js
@@ -26,64 +26,91 @@ class CharacterSheet extends Component {
             <AttributeBox name="size" value={0} modifier={true} />
             <AttributeBox name="reach" value={0} modifier={true} />
           </div>
-          <div className="character_sheet__body__defense">
-            <h5>Defense</h5>
-            <div className="character_sheet__body__defense__stat">
-              <h4>
-                <span className="character_sheet__background_text">value</span>(
-                <span className="character_sheet__background_text">max</span>)
-              </h4>
-            </div>
-            <div className="character_sheet__body__defense__bonuses__container">
-              <AttributeBox name="Melee Bonus" value={5} />
-              <AttributeBox name="Range Bonus" value={5} />
-              <AttributeBox name="Magic Bonus" value={5} />
-            </div>
-          </div>
-          <div className="character_sheet__body__stamina">
-            <h5>Stamina</h5>
-            <div className="character_sheet__body__stamina__stat">
-              <h4>
-                <span className="character_sheet__background_text">
-                  remaining
-                </span>
-                /
-                <span className="character_sheet__background_text">
-                  {" "}
-                  out of{" "}
-                </span>
-                (<span className="character_sheet__background_text">max</span>)
-              </h4>
-            </div>
-          </div>
-          <div className="character_sheet__body__wounds">
-            <div className="character_sheet__body__wounds__tracker">
-              <h5>Wounds</h5>
-              <div className="character_sheet__body__wounds__stat">
-                <h4>
-                  <span className="character_sheet__background_text">
-                    remaining
-                  </span>
-                  /
-                  <span className="character_sheet__background_text">
-                    {" "}
-                    out of{" "}
-                  </span>
-                </h4>
+          <div className="character_sheet__body__content">
+            <div className="character_sheet__body__resources">
+              <div className="character_sheet__body__resource">
+                <div className="character_sheet__body__resource__tracker">
+                  <h5>Defense</h5>
+                  <div className="character_sheet__body__resource__stat">
+                    <h4>
+                      <span className="character_sheet__background_text">
+                        current
+                      </span>
+                      (
+                      <span className="character_sheet__background_text">
+                        max
+                      </span>
+                      )
+                    </h4>
+                  </div>
+                </div>
+                <div className="character_sheet__body__resource__levels">
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Melee Bonus</h6>
+                    <input type="text" />
+                  </div>
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Range Bonus</h6>
+                    <input type="text" />
+                  </div>
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Magic Bonus</h6>
+                    <input type="text" />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="character_sheet__body__wounds__levels">
-              <div className="character_sheet__body__wounds__levels__level">
-                <h6>Wounded</h6>
-                <input type="checkbox" />
+              <div className="character_sheet__body__resource">
+                <div className="character_sheet__body__resource__tracker">
+                  <h5>Stamina</h5>
+                  <div className="character_sheet__body__resource__stat">
+                    <h4>
+                      <span className="character_sheet__background_text">
+                        remaining
+                      </span>
+                      /
+                      <span className="character_sheet__background_text">
+                        {" "}
+                        out of{" "}
+                      </span>
+                      (
+                      <span className="character_sheet__background_text">
+                        max
+                      </span>
+                      )
+                    </h4>
+                  </div>
+                </div>
               </div>
-              <div className="character_sheet__body__wounds__levels__level">
-                <h6>Badly Wounded</h6>
-                <input type="checkbox" />
-              </div>
-              <div className="character_sheet__body__wounds__levels__level">
-                <h6>Mortaly Wounded</h6>
-                <input type="checkbox" />
+              <div className="character_sheet__body__resource">
+                <div className="character_sheet__body__resource__tracker">
+                  <h5>Wounds</h5>
+                  <div className="character_sheet__body__resource__stat">
+                    <h4>
+                      <span className="character_sheet__background_text">
+                        remaining
+                      </span>
+                      /
+                      <span className="character_sheet__background_text">
+                        {" "}
+                        max{" "}
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+                <div className="character_sheet__body__resource__levels">
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Wounded</h6>
+                    <input type="checkbox" />
+                  </div>
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Badly Wounded</h6>
+                    <input type="checkbox" />
+                  </div>
+                  <div className="character_sheet__body__resource__levels__level">
+                    <h6>Mortaly Wounded</h6>
+                    <input type="checkbox" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
