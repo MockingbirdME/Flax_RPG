d2ca2b0ed055fc01ec314a050382aa408f4b06fa
Devon Henegar <devon@Devons-MacBook-Pro.local>
2019-06-09 16:13:52 -0400

    Autoformated topNav.

--- client/src/topNav/topNav.js
+++ client/src/topNav/topNav.js
@@ -1,45 +1,43 @@
-import React, {Component} from 'react';
-import './topNav.css'
-import NavLink from './navLink.js';
-import ExtendedNav from './extendedNav.js'
+import React, { Component } from "react";
+import "./topNav.css";
+import NavLink from "./navLink.js";
+import ExtendedNav from "./extendedNav.js";
 
 class TopNav extends Component {
-    state = {
-        hovering: null,
-        hoverDisplay: null
-    };
+  state = {
+    hovering: null,
+    hoverDisplay: null
+  };
 
-    onHover = (target) => {
-        this.setState({hovering: target}, () => {
-            if (target) this.setHoverDisplay();
-            else setTimeout(() => {this.setHoverDisplay()}, 100);
-        });
-    }
-    setHoverDisplay = () => {
-        this.setState({hoverDisplay: this.state.hovering});
-    }
+  onHover = target => {
+    this.setState({ hovering: target }, () => {
+      if (target) this.setHoverDisplay();
+      else
+        setTimeout(() => {
+          this.setHoverDisplay();
+        }, 100);
+    });
+  };
 
-    render() {
-        return (
-            <div className="topNav" >
-                <ul className="topNav__list">
-                    <NavLink
-                        title="Rules"
-                        onHover={this.onHover}
-                    />
-                    <NavLink
-                        title="Coming Soon"
-                        onHover={this.onHover}
-                     />
-                </ul>
-                <ExtendedNav
-                    hoverDisplay={this.state.hoverDisplay}
-                    onHover={this.onHover}
-                    title={"Rules"}
-                />
-            </div>
-        )
-    }
+  setHoverDisplay = () => {
+    this.setState({ hoverDisplay: this.state.hovering });
+  };
+
+  render() {
+    return (
+      <div className="topNav">
+        <ul className="topNav__list">
+          <NavLink title="Rules" onHover={this.onHover} />
+          <NavLink title="Character Sheet" onHover={this.onHover} />
+        </ul>
+        <ExtendedNav
+          hoverDisplay={this.state.hoverDisplay}
+          onHover={this.onHover}
+          title={"Rules"}
+        />
+      </div>
+    );
+  }
 }
 
 export default TopNav;
