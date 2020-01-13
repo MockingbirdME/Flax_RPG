933403fbe99ebf78db1b20daae7fe3aebc63cd17
Devon Henegar <devon.henegar@ibm.com>
2020-01-13 13:37:28 -0500

    Make Title "Flax" a link to "/"

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 class Header extends Component {
@@ -10,7 +11,7 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1 className="header__title">Flax</h1>
+                <h1 className="header__title"><Link to="/" className="header__link" >Flax</Link></h1>
             </div>
         );
     }
