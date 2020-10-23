98dedd4fbf2037db6b2a155b903f3cbce560cd6a
Devon Henegar <devon.henegar@ibm.com>
2020-10-23 13:39:24 -0400

    Move and improve resistances display

--- client/src/characterSheet/characterSheetDamage.js
+++ client/src/characterSheet/characterSheetDamage.js
@@ -1,5 +1,7 @@
 import React, { } from "react";
 import ResourceDisplay from "./resourceDisplay";
+import AttributeStat from "./attributeStat";
+
 
 const CharacterSheetStaminaAndWounds = props => {
   const maxStamina = (props.otherAttributes && props.otherAttributes.staminaMax) || 0;
@@ -8,31 +10,53 @@ const CharacterSheetStaminaAndWounds = props => {
   const currentMaxStamina = maxStamina - fatigue;
   const currentWounds = (props.otherAttributes && props.otherAttributes.woundsCurrent) || 0;
   const maxWounds = (props.otherAttributes && props.otherAttributes.woundsMax) || 1;
+
+  const resistancesList = Object.keys(props.otherAttributes.resistances);
+  
+  const resistances = (
+    <div className="" style={{flexGrow: 1}} >
+      <h5 className="character_sheet__body__resource_name">
+        Resistances:
+      </h5>
+      <div className="character_sheet__body__content__container__row character_sheet__body__group_wrap">
+        {resistancesList.map(key => (
+          <div className="character_sheet__body__content__container__row character_sheet__body__group__item__levels__level character_sheet__damage_resistance_item" style={resistancesList.length > 3 ? {width: "8rem"} : {width: "10rem"}} key={key}>
+            <h6>{key.charAt(0).toUpperCase() + key.slice(1)}</h6>
+            <AttributeStat style={resistancesList.length > 3 ? {width: "1.5rem"} : {width: "2.5rem"}} blankSheet={props.blankSheet} stats={props.otherAttributes} target={["resistances", key]} />
+          </div>
+        ))}
+      </div>
+      
+    </div>
+  );
   
   return (
     <div className="character_sheet__body__content__outer__container">
-      <div className="character_sheet__body__content__container__column" >
-        <h4 style={{margin: "0 0 -1.5rem"}}>Damage</h4>
+      <div className="character_sheet__body__content__container__row">
         <div className="character_sheet__body__content__container__row">
-          <ResourceDisplay name="Stamina" current={currentStamina} max={currentMaxStamina} />
-          <ResourceDisplay name="Fatigue" current={fatigue} />
-          <ResourceDisplay name="Wounds" current={currentWounds} max={maxWounds} />
-        
-          <div className="character_sheet__body__group__item__levels" >
+          <div className="character_sheet__body__group__item__levels character_sheet__damage_resource_container">
+            <ResourceDisplay name="Stamina" current={currentStamina} max={currentMaxStamina} />
             <div className="character_sheet__body__group__item__levels__level">
               <h6>Wounded</h6>
               <input type="checkbox" />
             </div>
+          </div>
+          <div className="character_sheet__body__group__item__levels character_sheet__damage_resource_container">
+            <ResourceDisplay name="Fatigue" current={fatigue} />
             <div className="character_sheet__body__group__item__levels__level">
               <h6>Badly Wounded</h6>
               <input type="checkbox" />
             </div>
+          </div>
+          <div className="character_sheet__body__group__item__levels character_sheet__damage_resource_container">
+            <ResourceDisplay name="Wounds" current={currentWounds} max={maxWounds} />
             <div className="character_sheet__body__group__item__levels__level">
               <h6>Mortaly Wounded</h6>
               <input type="checkbox" />
             </div>
           </div>
         </div>
+        {resistances}
       </div>
     </div>
     
